367f83b7fbe0b597b656113dcf19f6a56938d825
jonay2000 <jonabent@gmail.com>
2021-07-18 09:26:10 +0200

    make error handling better

--- src/device.rs
+++ src/device.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use thiserror::Error;
 use std::fmt::{Display, Formatter};
 
 pub mod ram;
@@ -8,37 +8,19 @@ pub mod device_map;
 pub use ram::Ram;
 pub use rom::Rom;
 
-#[derive(Debug, Ord, PartialOrd, Eq, PartialEq, Copy, Clone)]
+#[derive(Debug, Error, Eq, PartialEq, Copy, Clone)]
 pub enum WriteError {
+    #[error("not writable")]
     NotWritable,
+    #[error("invalid address")]
     InvalidAddress,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum CreateError {
-    FsError(std::io::Error),
+    #[error("filesystem error: {0}")]
+    FsError(#[from] std::io::Error),
 }
-
-impl From<std::io::Error> for CreateError {
-    fn from(e: std::io::Error) -> Self {
-        Self::FsError(e)
-    }
-}
-
-impl Display for CreateError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
-impl Error for CreateError {}
-
-impl Display for WriteError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
-
-impl Error for WriteError {}
 
 pub trait Device {
     fn read(&self, address: u16) -> Option<u8>;
